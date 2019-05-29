36edc242b88f908d8bca6a779218e8f0b3223bbe
GlebRed <glebredko@gmail.com>
2019-05-29 16:38:32 +0300

    Closes centre-for-educational-technology/enrope_theme#56

---
+++ enrope_bibliography/README.md
@@ -0,0 +1,1 @@
+# Enrope Annotated bibliography
---
+++ enrope_bibliography/src/Plugin/Block/AddBibliographyBlock.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\enrope_bibliography\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Url;
+
+/**
+ * Provides a block with operations the user can perform on a group.
+ *
+ * @Block(
+ *   id = "add_annotated_bibliography_block",
+ *   admin_label = @Translation("Add annotated bibliography block"),
+ * )
+ */
+class AddBibliographyBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+
+    $build['link'] = [
+        '#type' => 'link',
+        '#title' => 'Add annotated bibliography',
+        '#url' => Url::fromRoute('node.add', ['node_type' => 'annotated_bibliography']),
+        '#attributes' => [
+            'class' => ['btn btn-success mb-4'],
+        ],
+    ];
+
+
+
+    // If no group was found, cache the empty result on the route.
+    return $build;
+  }
+
+
+
+}
+
+
